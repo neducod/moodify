1500204a182a8b4825545cb5a57ba8c134ec2ca6
neducod <nedunony16@gmail.com>
2026-08-02 23:16:02 +0100

    hero section code

--- app/page.tsx
+++ app/page.tsx
@@ -69,6 +69,8 @@ export default function MaximalistMoodify() {
   };
 
   return (
+    <>
+    <section>fgfgfgfgf</section>
     <main
       className="min-h-screen transition-all duration-700 ease-out text-white p-6 md:p-12 relative overflow-hidden"
       style={{
@@ -312,5 +314,6 @@ export default function MaximalistMoodify() {
 
       </div>
     </main>
+    </>
   );
 }
